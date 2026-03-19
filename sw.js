3ba841e431129a256a368ba1d1e84127613279de
dsingson5 <dsingson5@users.noreply.github.com>
2026-03-19 03:52:13 +0000

    Deploy beta from Shiney-MBP development branch dsingson5/Shiney-MBP@328b96fd08f5831b111d42e4f94522061c19a64d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mbp-cache-v1772752235";
+const CACHE_NAME = "mbp-cache-v1773892330";
 const HEAVY_ASSETS = /\.(wasm|whl)(\?|$)/;
 const LIGHT_ASSETS = /\.(js|json|css|ico)(\?|$)/;
 self.addEventListener("install", function(e){ self.skipWaiting(); });
